ab9ee279ac330553e88e5fbb7fa9b775a973fa98
Franco Bellucci <belluccifranco@gmail.com>
2018-05-28 01:59:52 -0300

    Initial commit

---
+++ src/main/java/in/kiranreddy/EmailController.java
@@ -0,0 +1,24 @@
+package in.kiranreddy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class EmailController {
+
+    private MyEmailService emailService;
+
+    @Autowired
+    public EmailController(MyEmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/sendEmail")
+    @ResponseBody
+    public String sendEmail() {
+        emailService.sendMail("belluccifranco@gmail.com","Test subject","Test mail");
+        return "OK!";
+    }
+}
---
+++ src/main/java/in/kiranreddy/MyEmailService.java
@@ -0,0 +1,26 @@
+package in.kiranreddy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyEmailService {
+
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public MyEmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail(String toEmail, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(toEmail);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        mailMessage.setFrom("ventas@globodistribuciones.com");
+        javaMailSender.send(mailMessage);
+    }
+}
