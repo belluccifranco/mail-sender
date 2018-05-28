970928a9c361d74126eb8bd242003641f278665c
Franco Bellucci <belluccifranco@gmail.com>
2018-05-28 19:57:05 -0300

    Agrega async al metodo que envia mail

--- src/main/java/in/kiranreddy/EmailsenderApplication.java
+++ src/main/java/in/kiranreddy/EmailsenderApplication.java
@@ -2,8 +2,10 @@ package in.kiranreddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class EmailsenderApplication {
 
 	public static void main(String[] args) {
--- src/main/java/in/kiranreddy/MyEmailService.java
+++ src/main/java/in/kiranreddy/MyEmailService.java
@@ -1,8 +1,10 @@
 package in.kiranreddy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +17,8 @@ public class MyEmailService {
         this.javaMailSender = javaMailSender;
     }
 
-    public void sendMail(String toEmail, String subject, String message) {
+    @Async
+    public void sendMail(String toEmail, String subject, String message) throws MailException {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(toEmail);
         mailMessage.setSubject(subject);
